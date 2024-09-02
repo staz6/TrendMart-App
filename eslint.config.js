e303a287726a116d0749ed6b55e17375c63398d7
Staz6 <61322266+staz6@users.noreply.github.com>
2024-09-02 21:45:47 +0500

    chore: configured Husky with eslint,commitlint,prettier and typecheck

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended,eslintConfigPrettier,],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -19,10 +20,16 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "@typescript-eslint/ban-ts-comment": "off",
+      "comma-dangle": "off",
+      "multiline-ternary": "off",
+      "no-use-before-define": "off",
+      "space-before-function-paren": "off",
+      "react/prop-types": "off",
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/exhaustive-deps": "error",
     },
   },
 )
