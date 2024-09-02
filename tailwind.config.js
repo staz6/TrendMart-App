9eedf30d2d8bcd7c779f168c9026cd8e8b8c8089
Staz6 <61322266+staz6@users.noreply.github.com>
2024-09-02 22:24:19 +0500

    chore: integrate custom color palette and clean up default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        primary1: "var(--color-primary1)",
+        secondary: "var(--color-secondary)",
+        secondary1: "var(--color-secondary1)",
+        secondary2: "var(--color-secondary2)",
+        bg: "var(--color-bg)",
+        text: "var(--color-text)",
+        text1: "var(--color-text1)",
+        text2: "var(--color-text2)",
+        button: "var(--color-button)",
+        button1: "var(--color-button1)",
+        button2: "var(--color-button2)",
+        hoverbutton1: "var(--color-hoverbutton1)",
+        hoverbutton2: "var(--color-hoverbutton2)",
+      },
+    },
   },
   plugins: [],
 };
