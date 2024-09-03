b0886d2aa41f181bfd68d75bef89a3653d4c3734
Staz6 <61322266+staz6@users.noreply.github.com>
2024-09-03 23:52:47 +0500

    feat: implemented react router to navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import "./App.css";
 import { Header } from "./components/complex/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+  },
+  {
+    path: "/Contact",
+    element: <Header />,
+  },
+  {
+    path: "/Sign_Up",
+    element: <Header />,
+  },
+  {
+    path: "/About",
+    element: <Header />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Header />
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/components/complex/Header.tsx
+++ src/components/complex/Header.tsx
@@ -3,23 +3,50 @@ import Button from "../shared/Button";
 import Searchbar from "../compound/Searchbar";
 import Heart from "../../assets/WishListIcon.svg";
 import Cart from "../../assets/Cart.svg";
+import { NavLink } from "react-router-dom";
 export const Header: React.FC = () => {
   return (
     <div className="w-full  margin-auto lg:px-[5%] xl:px-32 pt-10 pb-5 border border-b-black border-opacity-30 shadow-sm">
       <div className="flex text-text2 justify-between items-center">
-        <a
-          href=""
+        <NavLink
+          to="/"
           className="text-2xl font-extrabold tracking-wider font-inter"
         >
           Exclusive
-        </a>
+        </NavLink>
         <div className="flex gap-5 xl:gap-10 text-md font-medium font-poppins">
-          <a href="" className="border-b border-b-black">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `border-b-2 ${isActive ? "border-black" : "border-transparent"} hover:border-black hover:shadow-md transition-all duration-300 `
+            }
+          >
             Home
-          </a>
-          <a href="">Contact</a>
-          <a href="">About</a>
-          <a href="">Sign Up</a>
+          </NavLink>
+          <NavLink
+            to="/Contact"
+            className={({ isActive }) =>
+              `border-b-2 ${isActive ? "border-black" : "border-transparent"} hover:border-black hover:shadow-md transition-all duration-300 `
+            }
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            to="/About"
+            className={({ isActive }) =>
+              `border-b-2 ${isActive ? "border-black" : "border-transparent"} hover:border-black hover:shadow-md transition-all duration-300 `
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/Sign_Up"
+            className={({ isActive }) =>
+              `border-b-2 ${isActive ? "border-black" : "border-transparent"} hover:border-black hover:shadow-md transition-all duration-300 `
+            }
+          >
+            Sign Up
+          </NavLink>
         </div>
         <div className="flex gap-5">
           <Searchbar />
