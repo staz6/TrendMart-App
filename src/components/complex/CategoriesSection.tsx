397f836dda4344a1ab8fd1233a4731b2c11a48da
Staz6 <61322266+staz6@users.noreply.github.com>
2024-09-18 22:06:37 +0500

    feat: add CategoriesSection feature and update section tests

---
+++ src/components/complex/CategoriesSection.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { IoIosPhonePortrait } from "react-icons/io";
+import CategoriesItem from "../compound/CategoriesItem";
+import Section from "./Section";
+
+export const CategoriesSection: React.FC = () => {
+  return (
+    <Section
+      breakpoints={{
+        0: { slidesPerView: 1 },
+        640: { slidesPerView: 4 },
+        830: { slidesPerView: 5 },
+        1280: { slidesPerView: 6 },
+      }}
+      sectionTitle="Categories"
+      sliderTitle="Browse By Category"
+      showTimer={false}
+      showPagination={false}
+    >
+      {Array.from({ length: 8 }).map((_, index) => (
+        <CategoriesItem key={index} icon={<IoIosPhonePortrait size={50} />} />
+      ))}
+    </Section>
+  );
+};
--- src/components/complex/__test__/Section.test.tsx
+++ src/components/complex/__test__/Section.test.tsx
@@ -19,7 +19,28 @@ jest.mock("../../compound/SectionSliderControls", () => () => (
 jest.mock(
   "../../compound/SectionSliderContent",
   () =>
-    ({ children }: { children: React.ReactNode }) => <div>{children}</div>,
+    ({
+      children,
+      spaceBetween,
+      breakpoints,
+      loop,
+    }: {
+      children: React.ReactNode;
+      spaceBetween: number;
+      breakpoints: {
+        [key: number]: {
+          slidesPerView: number;
+        };
+      };
+      loop: boolean;
+    }) => (
+      <div>
+        <div data-testid="spaceBetween">{spaceBetween}</div>
+        <div data-testid="breakpoints">{JSON.stringify(breakpoints)}</div>
+        <div data-testid="loop">{loop.toString()}</div>
+        <div>{children}</div>
+      </div>
+    ),
 );
 
 describe("Section component", () => {
@@ -29,7 +50,7 @@ describe("Section component", () => {
   it("renders the section title and slider title", () => {
     render(
       <Section
-        showPagination
+        showPagination={false}
         sectionTitle={sectionTitle}
         sliderTitle={sliderTitle}
         showTimer={false}
@@ -41,11 +62,11 @@ describe("Section component", () => {
 
     expect(screen.getByText(sectionTitle)).toBeInTheDocument();
     expect(screen.getByText(sliderTitle)).toBeInTheDocument();
-
+    expect(screen.queryByText("View All Products")).not.toBeInTheDocument();
     expect(screen.queryByText("Countdown Timer")).not.toBeInTheDocument();
   });
 
-  it("renders the CountdownTimer when showTimer is true", () => {
+  it("renders the CountdownTimer when showTimer and showPagination is true", () => {
     render(
       <Section
         showPagination
@@ -59,6 +80,7 @@ describe("Section component", () => {
     );
 
     expect(screen.getByText("Countdown Timer")).toBeInTheDocument();
+    expect(screen.getByTestId("paginationProductsSection")).toBeInTheDocument();
   });
 
   it("renders the slider controls and content", () => {
@@ -76,5 +98,33 @@ describe("Section component", () => {
 
     expect(screen.getByText("Slider Controls")).toBeInTheDocument();
     expect(screen.getByText("Product Card 2")).toBeInTheDocument();
+    expect(screen.getByText("Product Card 1")).toBeInTheDocument();
+  });
+  it("renders with custom spaceBetween, breakpoints, and loop values", () => {
+    const customBreakpoints = {
+      500: { slidesPerView: 1 },
+      900: { slidesPerView: 3 },
+    };
+
+    render(
+      <Section
+        showPagination={false}
+        sectionTitle={sectionTitle}
+        sliderTitle={sliderTitle}
+        showTimer={false}
+        spaceBetween={20}
+        breakpoints={customBreakpoints}
+        loop={false}
+      >
+        <div>Product Card 1</div>
+        <div>Product Card 2</div>
+      </Section>,
+    );
+
+    expect(screen.getByTestId("spaceBetween")).toHaveTextContent("20");
+    expect(screen.getByTestId("breakpoints")).toHaveTextContent(
+      JSON.stringify(customBreakpoints),
+    );
+    expect(screen.getByTestId("loop")).toHaveTextContent("false");
   });
 });
